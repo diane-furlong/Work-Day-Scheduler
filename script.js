067d363a3819d6415e62cd28f32d747abcea39de
Diane Furlong <dfurlongm@gmail.com>
2020-12-23 00:34:20 -0500

    added and styled textboxes, styled left column

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 $(document).ready(function() {
-let dateAtTop = document.getElementById("todaysDate")
-let todaysDate = document.createTextNode(moment().format('MMM Do YYYY, h:mm:ss a'));
-dateAtTop.appendChild(todaysDate);
-//moment().format('MMM Do YYYY, h:mm:ss a');
+
+    function a(){
+        let dateAtTop = $("#currentDay");
+        let todaysDate = $(moment().format('MMM Do YYYY, h:mm:ss a'));
+        dateAtTop.appendChild(todaysDate);
+        //moment().format('MMM Do YYYY, h:mm:ss a');
+    }
+
+    //style the times in the left column
+    let hourDisplay = $(".hour");
+    $(hourDisplay).width('100%').height('100%').css('border','1px solid black').css('text-align','center');
+
+    //style buttons in the right column
+    let saveButtons = $(".buttons");
+    //$(saveButtons).width(200).height(200);
+
+    //create the textboxes
+    let textbox = $("<input type = 'textbox'>")
+    $(textbox).width('100%').height('100%').css('border','1px solid black');
+    $(".textArea").append(textbox);
+
 })
